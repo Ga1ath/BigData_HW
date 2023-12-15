f42abe8841f9fb10b373ad6f907578c8ae7f35da
Ga1ath <vasjanovich_dmitry@mail.ru>
2023-12-15 22:10:24 +0300

    HomeWork 1.2

---
+++ 1_1/hw_1_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:109e37635983bce5d42af6279227b51f4ab48ca0ad895efd9cb7cebd0806f09b
+size 2827
---
+++ 1_2/hw_1_2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37c8b67944b96d366a8e4afc77be044b4c8e490bf94510f1b8c16e1cdf808471
+size 2446
