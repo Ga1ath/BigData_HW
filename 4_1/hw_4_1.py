629d3d782a6dd79e8ab58a4ae34ff043ae333f61
Ga1ath <vasjanovich_dmitry@mail.ru>
2024-01-12 14:46:57 +0300

    HomeWork 4.2

--- 4_1/hw_4_1.py
+++ 4_1/hw_4_1.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59de9cd6c553c550b975c4e3186aeb42f6a50f80fadf491e676f40185f2bd9ba
-size 3273
+oid sha256:4542d7c0c917952b0c183e996479177896bd69596e18a554b7d07b67e4d63066
+size 3193
---
+++ 4_2/hw_4_2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ced912aaed46ce8577fb0c71956ad953c2ddc731e697c80607ae200d951b5f98
+size 2448
